c26412b03ec971d2e58425f6d622e1acf5f150b9
shafiuzzaman-md <md.shafiuzzaman.hira@gmail.com>
2026-02-16 07:58:51 -0800

    artifacts clean up

--- vuln_artifacts/174_dict.c_541_local_cpp_cwe-125-oob-read/instrumented_source/build/CMakeFiles/LibXml2.dir/nanoftp.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/LibXml2.dir/nanoftp.c.o: \
-  /media/shafi/Research/STAILOR/se_runs/sailr_cegir/libxml2_62911_vul/174_dict.c_541_local_cpp_cwe-125-oob-read/instrumented_source/nanoftp.c \
-  /media/shafi/Research/STAILOR/se_runs/sailr_cegir/libxml2_62911_vul/174_dict.c_541_local_cpp_cwe-125-oob-read/instrumented_source/libxml.h \
-  /media/shafi/Research/STAILOR/se_runs/sailr_cegir/libxml2_62911_vul/174_dict.c_541_local_cpp_cwe-125-oob-read/instrumented_source/build/config.h \
-  /media/shafi/Research/STAILOR/se_runs/sailr_cegir/libxml2_62911_vul/174_dict.c_541_local_cpp_cwe-125-oob-read/instrumented_source/build/libxml/xmlversion.h \
-  /media/shafi/Research/STAILOR/se_runs/sailr_cegir/libxml2_62911_vul/174_dict.c_541_local_cpp_cwe-125-oob-read/instrumented_source/include/libxml/xmlexports.h
--- vuln_artifacts/174_dict.c_541_local_cpp_cwe-125-oob-read/instrumented_source/build/CMakeFiles/LibXml2Mod.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libxml2mod.a"
-)
